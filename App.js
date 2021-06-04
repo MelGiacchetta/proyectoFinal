6d37777c808ebab66ec5a106e34a716a94c454f7
jazmincavanagh <jazmincavanagh@hotmail.com>
2021-06-04 12:56:19 -0300

    ....

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import {
 } from 'react-native';
 import { Screen_ViewImportedCards } from './src/Screens/Screen_ViewImportedCards';
 import { Screen_Import } from './src/Screens/Screen_import';
-
+import { Sceen_FlatList } from './src/Screens/Screen_FlatList'
 class App extends Component {
     constructor(props){
       super(props);
---
+++ src/Screens/Screen_FlatList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import {
+    View, 
+    Text,
+    ProgressViewIOSComponent,
+} from 'react-native';
+
+export class Screen_FlatList extends component {
+    constructor(){
+        super();
+        this.state={
+            api: [],
+        }
+    }
+    
+    personas = [
+        {nombre: 'juan',apellido: 'Perez'},
+        {nombre: 'Ana',apellido: 'Fernandez'},
+        {nombre: 'Ernesto',apellido: 'Perez'},
+    ]
+}
+render(){
+    return(
+        <View style={{flex:1}}>
+            <FlatList
+            data = {this.state.api}
+            keyExtractor = { (item, idx) => idx.toString()}
+            renderItem= { ({item})=> 
+            <Text style={styles.texto}>{item.apellido},{item.nombre}</Text>}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    texto: {
+        fontSize: 20
+    }
+})
